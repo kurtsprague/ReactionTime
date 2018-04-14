1ad84325081e7bea121d23cf4fdc76d54d58fa7b
Kurt Sprague <34193116+kurtsprague@users.noreply.github.com>
2018-04-13 19:19:30 -0600

    Add files via upload

--- bcdcounter.v
+++ bcdcounter.v
@@ -1,24 +1,45 @@
-module BCDcounter(clk, reset, d);
+module BCDcounter(clk, enable, stop, count, ripplecarryout, BCDdecoderenable);
 
 input clk;
-input reset;
-output [3:0] d;
+input enable;
+input stop;
+output reg [3:0] count;
+output reg ripplecarryout;
+output reg BCDdecoderenable;
 
-reg [3:0] d;
+reg [3:0] d = 4'b0000;
+
 
 
 always@(posedge clk)
 	begin
-		if(reset)
-			d <= 0;
-		else if(d <= 9)
+		if(enable)
 			begin
-				d <= d + 1;
+			if(stop)
+				begin
+					count <= d;
+					BCDdecoderenable <= 1; 
+				end
+			else if(d < 4'b1001)
+				begin
+					d <= d + 1;
+					count <= d;
+					ripplecarryout <= 0;
+				end
+			else if(d == 4'b1001)
+				begin
+					ripplecarryout <= 1;
+					d <= 4'b0000;
+					count <= d;
+				end
 			end
-		else if (d == 9)
+		else
 			begin
 				d <= 0;
+				count <= 0;
+				ripplecarryout <= 0;
+				BCDdecoderenable <= 1;
 			end
-		end
+	end
 endmodule
 
