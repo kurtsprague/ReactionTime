0b9030a1317e556df3685c46ae6ef48ae05dac78
Andrew Zhu <36798911+andrewyzhu@users.noreply.github.com>
2018-04-15 19:55:39 -0600

    Add files via upload

--- lighter.v
+++ lighter.v
@@ -1,30 +1,18 @@
-// ====================================
-//   Ver  :| Authors					
-//   V1.1 :| Andrew Zhu & Kurt Sprague
-// ====================================
-
-//module to light up the led when its time
-//takes in a clock, an enable, a number to count up to
-// outputs an led trigger signal, and an enable
 module lighter(clk,enable, num, outled, enableout);
 
-//input/output declaration
 input clk;
-input [7:0] num;
+input [6:0] num;
 input enable;
 output reg enableout;
 output reg outled;
 
-//reg to hold bits
-reg [7:0] i = 8'h00;
+integer i = 0;
 
-//always at clock change
-//if enable is high count to given number
-//else trigger the output
+
 always@(posedge clk)
 	begin
 		if (enable) begin
-			if( i < num) begin
+			if( i < num*20) begin
 				i = i+1;
 			end
 			else begin
