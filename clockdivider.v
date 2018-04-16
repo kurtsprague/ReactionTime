0d7165b65f366432eef71672440134b9dac3afb6
Kurt Sprague <34193116+kurtsprague@users.noreply.github.com>
2018-04-15 19:15:28 -0600

    Add files via upload
    added comments

--- clockdivider.v
+++ clockdivider.v
@@ -1,3 +1,9 @@
+// ====================================
+//   Ver  :| Authors					
+//   V1.1 :| Andrew Zhu & Kurt Sprague
+// ====================================
+
+//module for "slowing" down clock using parameter and always@
 module clkdiv(clk, slowclk);
 input clk;
 output reg slowclk;
@@ -5,6 +11,8 @@ output reg slowclk;
 parameter pclk = 25000;
 integer i = 0;
 
+// the clock will be delayed by parameter size, interchangable
+// the delayed clock sends a pulse for every x pulses (defined by parameter)
 always@(posedge clk)
 begin 
 		if(i < pclk)
