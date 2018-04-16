0cbb2ae0fca428daed1e4998c73b20fadb534c1f
Kurt Sprague <34193116+kurtsprague@users.noreply.github.com>
2018-04-15 19:26:50 -0600

    Add files via upload
    added comments

--- lighter.v
+++ lighter.v
@@ -1,14 +1,26 @@
+// ====================================
+//   Ver  :| Authors					
+//   V1.1 :| Andrew Zhu & Kurt Sprague
+// ====================================
+
+//module to light up the led when its time
+//takes in a clock, an enable, a number to count up to
+// outputs an led trigger signal, and an enable
 module lighter(clk,enable, num, outled, enableout);
 
+//input/output declaration
 input clk;
 input [7:0] num;
 input enable;
 output reg enableout;
 output reg outled;
 
+//reg to hold bits
 reg [7:0] i = 8'h00;
 
-
+//always at clock change
+//if enable is high count to given number
+//else trigger the output
 always@(posedge clk)
 	begin
 		if (enable) begin
